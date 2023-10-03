b1c639f1bb0d1140b23e9c31c8911f0ddd871dd9
Thughes37 <143362238+Thughes37@users.noreply.github.com>
2023-10-03 01:45:50 +0000

    deleted line 6 and added lodash in line 7

--- TiffNode/node-crash-course/server.js
+++ TiffNode/node-crash-course/server.js
@@ -1,9 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
 
+  //lodash
+  const num= _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log('hello');
+  });
+
+  greet();
+  greet();
+
+  
 // set header content type
 res.setHeader('Content-Type', 'text/html');
 
@@ -38,16 +50,16 @@ fs.readFile(path, (err, data) => {
     if (err) {
         console.log(err);
         res.end();
-    } else {
+    } 
         //res.write(data);
-        res.statusCode = 200;
+        
         res.end(data);
     }
 })
 });
 
+//localhost is the default value for 2nd argument
 
-
-server.listen(3000, 'localhost', () =>{
+server.listen(3000, 'localhost', () => {
     console.log('listening for request on port 3000');
 });
